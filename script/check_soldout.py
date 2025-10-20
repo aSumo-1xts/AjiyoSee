a5e0063183a7d3c302d7b1f7586f5e6d496f703f
asumo <1xtelescope@gmail.com>
2025-10-20 13:35:03 +0900

    fix: 2回目の判定方法を修正

--- script/check_soldout.py
+++ script/check_soldout.py
@@ -1,46 +1,33 @@
 """
 check_soldout.py
 
+check_openclose.pyの実行結果がOPENの場合にのみ実行される。
 2回目のfilter_posts.pyの実行結果からSOLD OUT or STILL OPENを判定する。
 具体的には、posts_filtered_2nd.csvを読み込んでresult.txtを更新する。
-そもそもCLOSEである場合には実行されない。
 """
 
 import csv
 
-input_file_filtered_1st = "data/posts_filtered_1st.csv"
-input_file_filtered_2nd = "data/posts_filtered_2nd.csv"
-output_file = "data/result.txt"
-result = 1  # 1: OPEN, 2: SOLD OUT
+input_file = "posts_filtered_2nd.csv"
+output_file = "result.txt"
+result = 1 # 1: OPEN, 2: SOLD OUT
 
-# posts_filtered_1st.csvとposts_filtered_2nd.csvを読み込む
-with (
-    open(input_file_filtered_1st, newline="", encoding="utf-8") as f_filtered_1st,
-    open(input_file_filtered_2nd, newline="", encoding="utf-8") as f_filtered_2nd,
-):
-    reader_1st = csv.reader(f_filtered_1st)
-    reader_2nd = csv.reader(f_filtered_2nd)
-    rows_1st = list(reader_1st)
-    rows_2nd = list(reader_2nd)
+# posts_filtered_2nd.csvを読み込む
+with open(input_file, encoding="utf-8") as f:
+    reader = list(csv.reader(f))
+    data_rows = reader[1:]  # ヘッダーをスキップ
 
-    # 新たに投稿が無ければ完売していないと判定
-    if rows_1st[1][1] == rows_2nd[1][1]:
-        result = 1
-    else:
-        # ヘッダーから"id"列のインデックスを取得
-        id_idx_1st = rows_1st[0].index("id")
-        id_idx_2nd = rows_2nd[0].index("id")
-        # ヘッダーから"text"列のインデックスを取得
-        text_idx_2nd = rows_2nd[0].index("text")
+    # "text"列のインデックスを取得
+    header = reader[0]
+    try:
+        text_idx = header.index("text")
+    except ValueError:
+        raise Exception('"text"列が見つかりません')
 
-        # idの列を比較して、2回目にしか無い行のindexを取得
-        diff_indices = [
-            idx
-            for idx, row in enumerate(rows_2nd[1:], start=1)
-            if row[id_idx_2nd] not in [r[id_idx_1st] for r in rows_1st[1:]]
-        ]
-        # その行のtext列を調べて、「完売」が含まれていれば売り切れと判定
-        if any("完売" in rows_2nd[idx][text_idx_2nd] for idx in diff_indices):
+    # 時系列に沿って降順に走査
+    for row in reversed(data_rows):
+        text = row[text_idx]
+        if "完売" in text:
             result = 2
 
 if result == 2:
